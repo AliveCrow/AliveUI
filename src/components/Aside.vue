764f37e945beecc2291ef297ae90249fd66ab4e9
grit <136907108@qq.com>
2020-11-17 19:22:23 +0800

    封装nav和aslie组件

---
+++ src/components/Aside.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id='Aside_app'>
+    </div>
+</template>
+
+<script lang="ts">
+export default class{
+    name:'Aside'
+
+}
+</script>
+<style scoped lang='scss'>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id='Nav_app'>
+    </div>
+</template>
+
+<script lang="ts">
+export default class{
+    name:'Nav'
+
+}
+</script>
+<style scoped lang='scss'>
+
+</style>
